a7f06846aa3d5ff3a439f394487666e17d7eab44
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-15 21:47:02 -0400

    Initial commit
    Initializes the Ion game engine.

---
+++ src/main/java/com/brain/pulsar/Pulsar.java
@@ -0,0 +1,13 @@
+package main.java.com.brain.pulsar;
+
+import main.java.com.brain.ion.Ion;
+
+public class Pulsar {
+	
+	public static void main(String[] args) {
+		
+		Ion ion = new Ion();
+		
+	}
+	
+}
