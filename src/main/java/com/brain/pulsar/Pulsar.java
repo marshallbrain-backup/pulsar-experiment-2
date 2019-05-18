c29f98d1979904af51a5e325d74576291f630dff
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-17 22:21:48 -0400

    Display a gray box on screen

--- src/main/java/com/brain/pulsar/Pulsar.java
+++ src/main/java/com/brain/pulsar/Pulsar.java
@@ -1,6 +1,9 @@
 package main.java.com.brain.pulsar;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 
 import main.java.com.brain.ion.Ion;
 import main.java.com.brain.ion.RenderCall;
@@ -8,25 +11,40 @@ import main.java.com.brain.ion.TickCall;
 
 public class Pulsar implements TickCall, RenderCall {
 	
+	private Canvas screen;
+	
 	public Pulsar() {
 		
-		Canvas screen = new Canvas();
-		
+		screen = new Canvas();
 		Ion ion = new Ion(this, this, screen);
 		
-	}
-
-	@Override
-	public void render() {
+		init();
+		ion.start();
 		
-		// TODO Auto-generated method stub
+	}
+	
+	private void init() {
 		
 	}
 
 	@Override
 	public void tick() {
 		
-		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public  void render() {
+		
+		BufferStrategy bs = screen.getBufferStrategy();
+		Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(5, 5, 100, 100);
+		
+		g.dispose();
+		bs.show();
 		
 	}
 	
