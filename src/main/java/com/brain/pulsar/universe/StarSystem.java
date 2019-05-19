850ab2c757afef09fda45179568ea3d6b7a925f3
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-19 19:13:22 -0400

    Change constructor to receive a list of StarSystemTypes

--- src/main/java/com/brain/pulsar/universe/StarSystem.java
+++ src/main/java/com/brain/pulsar/universe/StarSystem.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class StarSystem {
 
-	public StarSystem(List<BodyType> typeBodys) {
+	public StarSystem(List<BodyType> typeBodys, List<StarSystemType> typeSytems) {
 		
+		StarSystemType systemType = typeSytems.get(0);
 		
+		System.out.println(systemType.getBody());
 		
 	}
 	
