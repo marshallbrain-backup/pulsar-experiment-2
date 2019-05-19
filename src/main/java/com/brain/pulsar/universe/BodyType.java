a4d7b2ed9c136d7f63c4ffd770f8bdebeb8f7d17
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-19 19:18:22 -0400

    Add more elements

--- src/main/java/com/brain/pulsar/universe/BodyType.java
+++ src/main/java/com/brain/pulsar/universe/BodyType.java
@@ -2,11 +2,24 @@ package main.java.com.brain.pulsar.universe;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.NormalizedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import main.java.com.brain.pulsar.files.format.Range;
 
 @XmlRootElement(name = "body")
 public class BodyType {
 	
 	@XmlElement
 	private String name;
+	@XmlElement
+	private String colonizable;
+	
+	@XmlElement
+	private Range radius;
+	@XmlElement(name = "temp_set")
+	private Range tempSet;
+	
+	
 	
 }
