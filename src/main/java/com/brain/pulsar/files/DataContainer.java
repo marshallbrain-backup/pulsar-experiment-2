ae53ea26d18a704a4996de8a9408c6f0e2fe43d3
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-19 14:41:58 -0400

    Add new constructor and new method
    The new constructor transfers all of the objects in the data variable
    from the list of DataContainers that is passed to it and puts them in
    its data variable.
    
    The new method getMatchData returns a list of whatever class is given to
    it containing all of the objects in the data variable that are an
    instance of that class.

--- src/main/java/com/brain/pulsar/files/DataContainer.java
+++ src/main/java/com/brain/pulsar/files/DataContainer.java
@@ -1,5 +1,6 @@
 package main.java.com.brain.pulsar.files;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAnyElement;
@@ -7,8 +8,35 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "pulsar")
 public class DataContainer {
-	
+
 	@XmlAnyElement(lax = true)
 	private List<Object> data;
+	
+	public DataContainer() {
+	}
+	
+	public DataContainer(List<DataContainer> dataList) {
+		
+		data = new ArrayList<>();
+		
+		for(DataContainer dc: dataList) {
+			data.addAll(dc.data);
+		}
+		
+	}
+	
+	public <T> List<T> getMatchData(Class<T> match) {
+		
+		List<T> matchingList = new ArrayList<>();
+		
+		for(Object o: data) {
+			if(match.isInstance(o)) {
+				matchingList.add(match.cast(o));
+			}
+		}
+		
+		return matchingList;
+		
+	}
 	
 }
