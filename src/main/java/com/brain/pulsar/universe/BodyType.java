5ae0566e23981f25791af20bed055c15a8c7c3af
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-19 23:36:11 -0400

    Add new methods and new constructor
    Added getName, getRandomTemp, getRandomRadius, and convert

--- src/main/java/com/brain/pulsar/universe/BodyType.java
+++ src/main/java/com/brain/pulsar/universe/BodyType.java
@@ -1,9 +1,9 @@
 package main.java.com.brain.pulsar.universe;
 
+import java.util.Random;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.NormalizedStringAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import main.java.com.brain.pulsar.files.format.Range;
 
@@ -20,6 +20,52 @@ public class BodyType {
 	@XmlElement(name = "temp_set")
 	private Range tempSet;
 	
+	Random random;
 	
+	public BodyType() {
+		random = new Random();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public long getRandomTemp() {
+		
+		double min = convert(tempSet.getMin());
+		double max = convert(tempSet.getMax());
+		
+		double t = ((max-min)*random.nextDouble())+min;
+		
+		return Math.round(t);
+		
+	}
+	
+	public long getRandomRadius() {
+		
+		double min = convert(radius.getMin());
+		double max = convert(radius.getMax());
+		
+		double r = ((max-min)*random.nextDouble())+min;
+		
+		return Math.round(r);
+		
+	}
+	
+	private double convert(String n) {
+		
+		String[] s = n.split("e");
+		
+		if(s.length == 1)
+			return Double.parseDouble(n);
+		
+		long e = Long.parseLong(s[1]);
+		String i = s[0];
+		
+		double d = Double.parseDouble(i);
+		
+		return d * Math.pow(10, e);
+		
+	}
 	
 }
