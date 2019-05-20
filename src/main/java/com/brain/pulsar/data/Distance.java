bb9a8950214140359d0b1f943c0cd5a5f6308eb6
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-20 16:48:44 -0400

    Add type setting and more methods
    Allows a DistanceType to be given to it that holds the data for how many
    meters that unit represents.
    
    Added the convert and equals methods.

--- src/main/java/com/brain/pulsar/data/Distance.java
+++ src/main/java/com/brain/pulsar/data/Distance.java
@@ -4,13 +4,46 @@ package main.java.com.brain.pulsar.data;
 public class Distance {
 	
 	private final long distance;
+	private final DistanceType type;
 	
-	public Distance(long d) {
+	public Distance(long d, DistanceType t) {
 		distance = d;
+		type = t;
 	}
 	
-	public long getDistance() {
-		return distance;
+	public Distance(Distance d) {
+		distance = d.distance;
+		type = d.type;
+	}
+
+	public double getDistance() {
+		return distance * type.getValue();
+	}
+
+	public Distance convert(DistanceType t) {
+		return new Distance(Math.round(distance * type.getValueInverse() * t.getValue()), t);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		if(o == this) {
+			return true;
+		}
+		
+		if(o instanceof Distance) {
+
+			Distance d1 = convert(DistanceType.METER);
+			Distance d2 = ((Distance) o).convert(DistanceType.METER);
+			
+			if(d1.distance == d2.distance && d1.type == d2.type && d1.type == DistanceType.METER) {
+				return true;
+			}
+			
+		}
+		
+		return false;
+		
 	}
 	
 }
