f5d741e5a9046e52120be6f79449716a2b6e8c62
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-20 14:13:25 -0400

    Initial commit
    Holds information about a distance.

---
+++ src/main/java/com/brain/pulsar/data/Distance.java
@@ -0,0 +1,16 @@
+package main.java.com.brain.pulsar.data;
+
+
+public class Distance {
+	
+	private final long distance;
+	
+	public Distance(long d) {
+		distance = d;
+	}
+	
+	public long getDistance() {
+		return distance;
+	}
+	
+}
