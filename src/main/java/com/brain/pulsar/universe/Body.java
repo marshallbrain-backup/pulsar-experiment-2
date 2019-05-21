81f8abda3673572d673a7dca4a83bebb1bc2cbd0
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-21 14:58:26 -0400

    Add more methods
    Added the following methods.
    
    - new constructor, takes a parent body and a distance. Used when the
    type of body is dependent on the temperature of the body.
    
    - nTemperatureCalc, calculates the temperature of a body by using the
    emission temperature of all the bodies in the system.
    
    - setType, sets the type of body based on the temperature of said body.
    
    - getDistance and getPolar, getting distances and coordinates between
    bodies.

--- src/main/java/com/brain/pulsar/universe/Body.java
+++ src/main/java/com/brain/pulsar/universe/Body.java
@@ -1,5 +1,7 @@
 package main.java.com.brain.pulsar.universe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import main.java.com.brain.pulsar.data.Distance;
@@ -8,12 +10,15 @@ import main.java.com.brain.pulsar.data.DistanceType;
 public class Body implements Cloneable {
 	
 	private long temperature;
+	private long temperatureEmission;
 	
 	private double angle;
 	
 	private Distance distance;
 	private Distance radius;
 	private Random random;
+	private Body parent;
+	private BodyType type;
 	
 	public Body() {
 		init();
@@ -22,27 +27,82 @@ public class Body implements Cloneable {
 	public Body(BodyType starType, Body core) {
 		init();
 		
-		temperature = starType.getRandomTemp();
-		radius = new Distance(starType.getRandomRadius(), DistanceType.SOLAR_RADIUS);
+		type = starType;
 		
-		angle = 0;
+		temperatureEmission = type.getRandomTemp();
+		radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
 		distance = new Distance(0, DistanceType.AU);
+		angle = 0;
+		
+		temperature = temperatureEmission;
 		
 	}
 
-	public Body(BodyType starType, Body parent, long d) {
+	public Body(BodyType starType, Body p, long d) {
 		init();
 		
-		temperature = starType.getRandomTemp();
-		radius = new Distance(starType.getRandomRadius(), DistanceType.SOLAR_RADIUS);
+		parent = p;
+		type = starType;
 		
-		angle = random.nextDouble() * 360;
+		temperature = type.getRandomTemp();
+		radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
 		distance = new Distance(d, DistanceType.AU);
+		angle = random.nextDouble() * 360;
+		
+		temperature = temperatureEmission;
 		
 	}
 	
+	public Body(Body p, int d) {
+		init();
+		
+		parent = p;
+
+		distance = new Distance(d, DistanceType.AU);
+		angle = random.nextDouble() * 360;
+		
+	}
+
 	private void init() {
 		random = new Random();
+	}
+
+	public void nTemperatureCalc(List<Body> bodyList) {
+		
+		for(Body b: bodyList) {
+			if(b != this && b.temperatureEmission > 0) {
+				
+				long t = b.temperatureEmission;
+				Distance d = Body.getDistance(this.clone(), b.clone());
+				Distance r = b.radius;
+				
+				double tem = t * Math.sqrt(r.getDistance()/(2*d.getDistance()));
+				temperature += Math.round(tem);
+				
+			}
+		}
+		
+	}
+
+	public void setType(List<BodyType> typeBodys) {
+		
+		if(type == null) {
+			
+			List<BodyType> sutable = new ArrayList<>();
+			
+			for(BodyType b: typeBodys) {
+				if(b.inTemperatureRange(temperature)) {
+					sutable.add(b);
+				}
+			}
+			
+			//TODO should be based on spawn chance
+			//isSuitable()
+			type = sutable.get(0);
+			radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
+			
+		}
+		
 	}
 
 	public Distance getRadius() {
@@ -60,16 +120,75 @@ public class Body implements Cloneable {
 	public double getAngle() {
 		return angle;
 	}
+
+	private static Distance getDistance(Body body1, Body body2) {
+		
+		double[] polar1 = getPolar(body1);
+		double[] polar2 = getPolar(body2);
+		
+		double[] polar = getPolar(polar1, polar2);
+		
+		return new Distance(polar[0], DistanceType.AU);
+		
+	}
+
+	private static double[] getPolar(Body body) {
+		
+		Body pare = body.parent;
+		
+		double[] polar1 = {body.distance.getDistance(), body.angle};
+		if(pare == null) {
+			return polar1;
+		}
+		double[] polar2 = {pare.distance.getDistance(), pare.angle};
+		
+		return getPolar(polar1, polar2);
+		
+	}
 	
+	private static double[] getPolar(double[] polar1, double[] polar2) {
+		
+		double r1 = polar1[0];
+		double r2 = polar2[0];
+		double t1 = polar1[1];
+		double t2 = polar2[1];
+		
+		double r = 
+				Math.sqrt(
+						( Math.pow(r1, 2) + Math.pow(r2, 2) ) + 
+						( (2)*(r1)*(r2) * Math.cos(t2 - t1) ) 
+						)
+				;
+		double t = 
+				t1 + Math.atan2(
+						r2 * Math.sin(t2 - t1),
+						r1 + r2 * Math.sin(t2 - t1)
+						)
+				;
+		
+		return new double[] {r, t};
+		
+	}
+
 	@Override
 	public Body clone() {
 		
-		Body clone = new Body();
-		
-		clone.angle = angle;
-		clone.distance = distance;
-		clone.radius = radius;
-		clone.temperature = temperature;
+		Body clone = null;
+		try {
+			
+			clone = (Body) super.clone();
+			
+			if(type != null) {
+				clone.type = type.clone();
+			}
+			
+			if(parent != null) {
+				clone.parent = parent.clone();
+			}
+			
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
 		
 		return clone;
 		
