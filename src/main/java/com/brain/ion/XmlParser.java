5cc2722a9790ddeab51cc836d8f6253dc0e38e23
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-21 18:10:06 -0400

    Allow both reading from a file and a string

--- src/main/java/com/brain/ion/XmlParser.java
+++ src/main/java/com/brain/ion/XmlParser.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -12,23 +14,39 @@ import javax.xml.bind.Unmarshaller;
 public class XmlParser {
 
 	public static Object getXml(File fileName, Class<?>[] classList) {
-		return readXmlFile(fileName, classList);
-	}
-	
-	private static Object readXmlFile(File fileName, Class<?>[] classList) {
 		
-		try(FileReader reader = new FileReader(fileName)) {
-			
-			JAXBContext context = JAXBContext.newInstance(classList);
-			Unmarshaller um = context.createUnmarshaller();
-			
-			return um.unmarshal(reader);
-			
-		} catch (IOException | JAXBException e) {
+		Object xml = null;
+		
+		try {
+			xml = readXmlFile(new FileReader(fileName), classList);
+		} catch (FileNotFoundException | JAXBException e) {
 			e.printStackTrace();
 		}
 		
-		return null;
+		return xml;
+		
+	}
+	
+	public static Object getXml(String xmlString, Class<?>[] classList) {
+		
+		Object xml = null;
+		
+		try {
+			xml = readXmlFile(new StringReader(xmlString), classList);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		return xml;
+		
+	}
+	
+	private static Object readXmlFile(Reader reader, Class<?>[] classList) throws JAXBException {
+			
+		JAXBContext context = JAXBContext.newInstance(classList);
+		Unmarshaller um = context.createUnmarshaller();
+		
+		return um.unmarshal(reader);
 		
 	}
 
