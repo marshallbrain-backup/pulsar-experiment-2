9797816e21f3e20143365f65ee5a27dad480c118
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-20 21:29:41 -0400

    Add new method
    Added getStarList that returns a list of clones of the stars that exist
    in the system.

--- src/main/java/com/brain/pulsar/universe/StarSystem.java
+++ src/main/java/com/brain/pulsar/universe/StarSystem.java
@@ -1,8 +1,11 @@
 package main.java.com.brain.pulsar.universe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StarSystem {
+	
+	private List<Body> starList = new ArrayList<>();
 
 	public StarSystem(List<BodyType> typeBodys, List<StarSystemType> typeSytems) {
 		
@@ -18,6 +21,19 @@ public class StarSystem {
 		Body core = new Body();
 		Body star = new Body(starType, core);
 		
+		starList.add(star);
+		
+	}
+
+	public List<Body> getStarList() {
+		
+		List<Body> list = new ArrayList<>();
+		
+		for(Body b: starList) {
+			list.add(b.clone());
+		}
+		
+		return list;
 	}
 	
 }
