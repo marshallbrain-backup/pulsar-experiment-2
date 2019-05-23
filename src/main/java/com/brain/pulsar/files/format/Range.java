6c8d9d7b7e88c7f18f001c2d3107aa1e4173cdee
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-23 17:11:29 -0400

    Cleanup code and add comments

--- src/main/java/com/brain/pulsar/files/format/Range.java
+++ src/main/java/com/brain/pulsar/files/format/Range.java
@@ -3,60 +3,42 @@ package main.java.com.brain.pulsar.files.format;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import main.java.com.brain.pulsar.universe.BodyType;
-
+/**
+ * @author Marshall Brain
+ *
+ */
 @XmlRootElement(name = "range")
 public class Range implements Cloneable {
 	
 	@XmlAttribute
-	private String min;
-	@XmlAttribute
 	private String max;
+	@XmlAttribute
+	private String min;
 	
+	/**
+	 * Empty constructor
+	 */
 	public Range() {
+	
 	}
 	
+	/**
+	 * @param min
+	 *            The minimum value
+	 * @param max
+	 *            The max value
+	 */
 	public Range(String min, String max) {
+		
 		this.min = min;
 		this.max = max;
 	}
-
-	public String getMin() {
-		return min;
-	}
 	
-	public String getMax() {
-		return max;
-	}
-
-	@Override
-	public boolean equals(Object object) {
-		
-		Boolean equals = false;
-		
-		if(object instanceof Range) {
-			
-			Range compare = (Range) object;
-			
-			if(this == compare) {
-				equals = true;
-			} else {
-				equals = true;
-				if(!min.equals(compare.min)) {
-					equals = false;
-				}
-				if(!max.equals(compare.max)) {
-					equals = false;
-				}
-				
-			}
-			
-		}
-		
-		return equals;
-		
-	}
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
 	@Override
 	public Range clone() {
 		
@@ -74,6 +56,57 @@ public class Range implements Cloneable {
 		
 		return clone;
 		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object object) {
+		
+		Boolean equals = false;
+		
+		if (object instanceof Range) {
+			
+			Range compare = (Range) object;
+			
+			if (this == compare) {
+				equals = true;
+			} else {
+				equals = true;
+				if (!min.equals(compare.min)) {
+					equals = false;
+				}
+				if (!max.equals(compare.max)) {
+					equals = false;
+				}
+				
+			}
+			
+		}
+		
+		return equals;
+		
+	}
+	
+	/**
+	 * Returns the max value
+	 * 
+	 * @return
+	 */
+	public String getMax() {
+		
+		return max;
+	}
+	
+	/**
+	 * @return Returns the minimum value
+	 */
+	public String getMin() {
+		
+		return min;
 	}
 	
 }
