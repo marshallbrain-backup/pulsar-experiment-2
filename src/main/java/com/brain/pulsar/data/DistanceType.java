9e62020c2c0ac29858a63d349553438118276968
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-23 14:42:46 -0400

    Cleanup code and add comments

--- src/main/java/com/brain/pulsar/data/DistanceType.java
+++ src/main/java/com/brain/pulsar/data/DistanceType.java
@@ -1,27 +1,48 @@
 package main.java.com.brain.pulsar.data;
 
-
+/**
+ * Stores information about conversions for different unit types
+ * 
+ * @author Marshall Brain
+ *
+ */
 public enum DistanceType {
 	
-	METER(1L, 0),
 	AU(149597870700L, 0),
-	SOLAR_RADIUS(695500L, 3),
-	;
+	METER(1L, 0),
+	SOLAR_RADIUS(695500L, 3);
 	
-	private final long number;
 	private final int exponent;
 	
+	private final long number;
+	
+	/**
+	 * @param number
+	 *            The number of meters that the unit represents
+	 * @param exponent
+	 *            The scientific notation that the number is in. Allows for
+	 *            decimals.
+	 */
 	private DistanceType(long n, int e) {
+		
 		number = n;
 		exponent = e;
 	}
 	
+	/**
+	 * @return A double representation of the unit in meters
+	 */
 	public double getValue() {
+		
 		return number * Math.pow(10, exponent);
 	}
-
+	
+	/**
+	 * @return The inverse of the number gotten from value
+	 */
 	public double getValueInverse() {
-		return 1/getValue();
+		
+		return 1 / getValue();
 	}
 	
 }
