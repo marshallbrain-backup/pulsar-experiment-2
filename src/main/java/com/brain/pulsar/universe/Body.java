4003806ab70d8644dca4cbd207e5d16e7033d759
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-23 20:24:13 -0400

    Cleanup code and add comments

--- src/main/java/com/brain/pulsar/universe/Body.java
+++ src/main/java/com/brain/pulsar/universe/Body.java
@@ -7,6 +7,12 @@ import java.util.Random;
 import main.java.com.brain.pulsar.data.Distance;
 import main.java.com.brain.pulsar.data.DistanceType;
 
+/**
+ * Stores information about a cosmic body.
+ * 
+ * @author Marshall Brain
+ *
+ */
 public class Body implements Cloneable {
 	
 	private long temperature;
@@ -14,19 +20,51 @@ public class Body implements Cloneable {
 	
 	private double angle;
 	
+	private Body parent;
 	private Distance distance;
 	private Distance radius;
 	private Random random;
-	private Body parent;
 	private BodyType type;
 	
 	public Body() {
+		
 		init();
 	}
-
-	public Body(BodyType starType, Body core) {
+	
+	/**
+	 * Generate a new body of unknown type. Used when generating bodes whose type is
+	 * dependent on the temperature.
+	 * 
+	 * @param parent
+	 *            The body that this is orbiting
+	 * @param distance
+	 *            The distance from the parent this is orbiting at
+	 */
+	public Body(Body p, int d) {
+		
 		init();
 		
+		parent = p;
+		
+		distance = new Distance(d, DistanceType.AU);
+		angle = random.nextDouble() * 360;
+		
+	}
+	
+	/**
+	 * Generates a new body based on the given type and centered on the given
+	 * parent.
+	 * 
+	 * @param starType
+	 *            The type of body to be based off of
+	 * @param parent
+	 *            The body that this is centered on
+	 */
+	public Body(BodyType starType, Body p) {
+		
+		init();
+		
+		parent = p;
 		type = starType;
 		
 		temperatureEmission = type.getRandomTemp();
@@ -37,14 +75,26 @@ public class Body implements Cloneable {
 		temperature = temperatureEmission;
 		
 	}
-
+	
+	/**
+	 * Generates a new body based on the given type and orbiting the given parent at
+	 * the given distance.
+	 * 
+	 * @param starType
+	 *            The type of body to be based off of
+	 * @param parent
+	 *            The body that this is orbiting
+	 * @param distance
+	 *            The distance from the parent this is orbiting at
+	 */
 	public Body(BodyType starType, Body p, long d) {
+		
 		init();
 		
 		parent = p;
 		type = starType;
 		
-		temperature = type.getRandomTemp();
+		temperatureEmission = type.getRandomTemp();
 		radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
 		distance = new Distance(d, DistanceType.AU);
 		angle = random.nextDouble() * 360;
@@ -53,75 +103,59 @@ public class Body implements Cloneable {
 		
 	}
 	
-	public Body(Body p, int d) {
-		init();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public Body clone() {
 		
-		parent = p;
-
-		distance = new Distance(d, DistanceType.AU);
-		angle = random.nextDouble() * 360;
-		
-	}
-
-	private void init() {
-		random = new Random();
-	}
-
-	public void nTemperatureCalc(List<Body> bodyList) {
-		
-		for(Body b: bodyList) {
-			if(b != this && b.temperatureEmission > 0) {
-				
-				long t = b.temperatureEmission;
-				Distance d = Body.getDistance(this.clone(), b.clone());
-				Distance r = b.radius;
-				
-				double tem = t * Math.sqrt(r.getDistance()/(2*d.getDistance()));
-				temperature += Math.round(tem);
-				
-			}
-		}
-		
-	}
-
-	public void setType(List<BodyType> typeBodys) {
-		
-		if(type == null) {
+		Body clone = null;
+		try {
 			
-			List<BodyType> sutable = new ArrayList<>();
+			clone = (Body) super.clone();
 			
-			for(BodyType b: typeBodys) {
-				if(b.inTemperatureRange(temperature)) {
-					sutable.add(b);
-				}
+			if (type != null) {
+				clone.type = type.clone();
 			}
 			
-			//TODO should be based on spawn chance
-			//isSuitable()
-			type = sutable.get(0);
-			radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
+			if (parent != null) {
+				clone.parent = parent.clone();
+			}
 			
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
 		}
 		
+		return clone;
+		
 	}
-
-	public Distance getRadius() {
-		return new Distance(radius);
-	}
-
-	public Distance getDistance() {
-		return new Distance(distance);
-	}
-
-	public long getTemperature() {
-		return temperature;
-	}
-
+	
+	/**
+	 * @return The angle the body is at.
+	 */
 	public double getAngle() {
+		
 		return angle;
 	}
-
-	private static Distance getDistance(Body body1, Body body2) {
+	
+	/**
+	 * @return The distance from the parent.
+	 */
+	public Distance getDistance() {
+		
+		return new Distance(distance);
+	}
+	
+	/**
+	 * Gets the distance between two bodies.
+	 * 
+	 * @param body1
+	 * @param body2
+	 * @return The distance between body1 and body2
+	 */
+	private Distance getDistance(Body body1, Body body2) {
 		
 		double[] polar1 = getPolar(body1);
 		double[] polar2 = getPolar(body2);
@@ -131,66 +165,118 @@ public class Body implements Cloneable {
 		return new Distance(polar[0], DistanceType.AU);
 		
 	}
-
-	private static double[] getPolar(Body body) {
+	
+	/**
+	 * @param body
+	 * @return A array of two doubles that are the distance and angle from the
+	 *         system core that the given planet is at
+	 */
+	private double[] getPolar(Body body) {
 		
 		Body pare = body.parent;
 		
-		double[] polar1 = {body.distance.getDistance(), body.angle};
-		if(pare == null) {
+		double[] polar1 = { body.distance.getDistance(), body.angle };
+		if (pare == null) {
 			return polar1;
 		}
-		double[] polar2 = {pare.distance.getDistance(), pare.angle};
+		double[] polar2 = { pare.distance.getDistance(), pare.angle };
 		
 		return getPolar(polar1, polar2);
 		
 	}
 	
-	private static double[] getPolar(double[] polar1, double[] polar2) {
+	/**
+	 * @param polar1
+	 * @param polar2
+	 * @return Adds the two polar coordinates together
+	 */
+	private double[] getPolar(double[] polar1, double[] polar2) {
 		
 		double r1 = polar1[0];
 		double r2 = polar2[0];
 		double t1 = polar1[1];
 		double t2 = polar2[1];
 		
-		double r = 
-				Math.sqrt(
-						( Math.pow(r1, 2) + Math.pow(r2, 2) ) + 
-						( (2)*(r1)*(r2) * Math.cos(t2 - t1) ) 
-						)
-				;
-		double t = 
-				t1 + Math.atan2(
-						r2 * Math.sin(t2 - t1),
-						r1 + r2 * Math.sin(t2 - t1)
-						)
-				;
+		double r = Math.sqrt((Math.pow(r1, 2) + Math.pow(r2, 2)) + ((2) * (r1) * (r2) * Math.cos(t2 - t1)));
+		double t = t1 + Math.atan2(r2 * Math.sin(t2 - t1), r1 + r2 * Math.sin(t2 - t1));
 		
-		return new double[] {r, t};
+		return new double[] { r, t };
 		
 	}
-
-	@Override
-	public Body clone() {
+	
+	/**
+	 * @return The radius of the body.
+	 */
+	public Distance getRadius() {
 		
-		Body clone = null;
-		try {
-			
-			clone = (Body) super.clone();
-			
-			if(type != null) {
-				clone.type = type.clone();
+		return new Distance(radius);
+	}
+	
+	/**
+	 * @return The temperature of the body.
+	 */
+	public long getTemperature() {
+		
+		return temperature;
+	}
+	
+	/**
+	 * Generic initialization code.
+	 */
+	private void init() {
+		
+		random = new Random();
+	}
+	
+	/**
+	 * Calculates the temperature of this body based on the emission temperature of
+	 * all other bodies in the system.
+	 * 
+	 * @param bodyList
+	 *            The list of bodies that are in the system
+	 */
+	public void nTemperatureCalc(List<Body> bodyList) {
+		
+		for (Body b : bodyList) {
+			if (b != this && b.temperatureEmission > 0) {
+				
+				long t = b.temperatureEmission;
+				Distance d = getDistance(this.clone(), b.clone());
+				Distance r = b.radius;
+				
+				double tem = t * Math.sqrt(r.getDistance() / (2 * d.getDistance()));
+				temperature += Math.round(tem);
+				
 			}
-			
-			if(parent != null) {
-				clone.parent = parent.clone();
-			}
-			
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
 		}
 		
-		return clone;
+	}
+	
+	/**
+	 * Sets the type of this body to one that has a temperature range that includes
+	 * this body temperature.
+	 * 
+	 * @param typeBodys
+	 *            The list of bodies that can be assigned
+	 */
+	public void setType(List<BodyType> typeBodys) {
+		
+		if (type == null) {
+			
+			List<BodyType> sutable = new ArrayList<>();
+			
+			for (BodyType b : typeBodys) {
+				if (b.inTemperatureRange(temperature)) {
+					sutable.add(b);
+				}
+			}
+			
+			// TODO should be based on spawn chance
+			// isSuitable()
+			type = sutable.get(0);
+			radius = new Distance(type.getRandomRadius(), DistanceType.SOLAR_RADIUS);
+			
+		}
 		
 	}
 	
