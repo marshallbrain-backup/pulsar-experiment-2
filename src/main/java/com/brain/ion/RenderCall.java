0a7950e899d88be09026162efaebe14754d6217f
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-24 19:16:20 -0400

    Changed comment wording

--- src/main/java/com/brain/ion/RenderCall.java
+++ src/main/java/com/brain/ion/RenderCall.java
@@ -1,7 +1,7 @@
 package main.java.com.brain.ion;
 
 /**
- * The interface that allows the main game loop to the render method of the
+ * The interface that allows the main game loop to call the render method of the
  * game. Add this to the class that should be called when it is time to render a
  * frame.
  * 
