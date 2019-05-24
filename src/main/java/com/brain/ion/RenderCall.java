e1f1d2c0cad6987e3d4f85aa215af0e7daab0baa
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-24 17:12:45 -0400

    Cleanup code and add comments

--- src/main/java/com/brain/ion/RenderCall.java
+++ src/main/java/com/brain/ion/RenderCall.java
@@ -1,9 +1,18 @@
 package main.java.com.brain.ion;
 
-import java.awt.Canvas;
-
+/**
+ * The interface that allows the main game loop to the render method of the
+ * game. Add this to the class that should be called when it is time to render a
+ * frame.
+ * 
+ * @author Marshall Brain
+ *
+ */
 public interface RenderCall {
 	
+	/**
+	 * The method that is called when the game should be rendered.
+	 */
 	void render();
 	
 }
