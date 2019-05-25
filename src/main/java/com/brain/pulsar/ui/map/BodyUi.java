6b1002a43cb9f10f0b5c0f5b56ab09b9a4217dff
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-25 15:27:38 -0400

    Draws the vectors from the vector group

--- src/main/java/com/brain/pulsar/ui/map/BodyUi.java
+++ src/main/java/com/brain/pulsar/ui/map/BodyUi.java
@@ -2,23 +2,35 @@ package main.java.com.brain.pulsar.ui.map;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.util.List;
 
+import main.java.com.brain.ion.graphics.VectorGraphics;
+import main.java.com.brain.ion.graphics.vectors.Vector;
+import main.java.com.brain.ion.graphics.vectors.VectorGroup;
 import main.java.com.brain.pulsar.universe.Body;
 
 public class BodyUi {
 	
 	private Body body;
+	private VectorGroup vectorLayer;
 
-	public BodyUi(Body b) {
+	public BodyUi(Body b, VectorGroup vg) {
 		
 		body = b;
+		vectorLayer = vg;
 		
 	}
 
-	public void render(Graphics2D g) {
+	public void render(VectorGraphics g) {
 		
-		g.setColor(Color.BLUE);
-		g.fillOval(0, 0, 50, 50);
+		List<Vector> vectors = vectorLayer.getVectors();
+		
+		for(Vector v: vectors) {
+			Shape s = v.getShape(50, 50);
+			g.draw(s);
+		}
 		
 	}
 	
