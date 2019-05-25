9acce4f490ae3c1ab8484cf2590c89fc3422ff8c
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-25 15:29:10 -0400

    Add new methods

--- src/main/java/com/brain/ion/graphics/vectors/Vector.java
+++ src/main/java/com/brain/ion/graphics/vectors/Vector.java
@@ -1,6 +1,15 @@
 package main.java.com.brain.ion.graphics.vectors;
 
+import java.awt.Shape;
 
-public interface Vector {
+public interface Vector extends Cloneable {
+
+	Vector clone();
+	
+	Shape getShape();
+
+	default Shape getShape(int x, int y) {
+		return getShape();
+	}
 	
 }
