b83485d68e31b8ee9aaa6ff90c0eed2648b93603
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-25 15:27:07 -0400

    Give BodyUi the corresponding vector and added VectorGraphics

--- src/main/java/com/brain/pulsar/ui/map/StarSystemUi.java
+++ src/main/java/com/brain/pulsar/ui/map/StarSystemUi.java
@@ -3,7 +3,12 @@ package main.java.com.brain.pulsar.ui.map;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import main.java.com.brain.ion.graphics.VectorGraphics;
+import main.java.com.brain.ion.graphics.vectors.Circle;
+import main.java.com.brain.ion.graphics.vectors.Vector;
+import main.java.com.brain.ion.graphics.vectors.VectorGroup;
 import main.java.com.brain.pulsar.universe.Body;
 import main.java.com.brain.pulsar.universe.StarSystem;
 
@@ -12,19 +17,20 @@ public class StarSystemUi {
 	private StarSystem system;
 	private List<BodyUi> bodyList;
 
-	public StarSystemUi(StarSystem mainSystem) {
+	public StarSystemUi(StarSystem mainSystem, Map<String, VectorGroup> bodys) {
 		
 		system = mainSystem;
 		
 		bodyList = new ArrayList<>();
 		
+		VectorGroup base = bodys.getOrDefault("", new VectorGroup());
 		for(Body b: system.getStarList()) {
-			bodyList.add(new BodyUi(b));
+			bodyList.add(new BodyUi(b, bodys.getOrDefault(b.getId(), base)));
 		}
 		
 	}
 
-	public void render(Graphics2D g) {
+	public void render(VectorGraphics g) {
 		
 		for(BodyUi b: bodyList) {
 			b.render(g);
