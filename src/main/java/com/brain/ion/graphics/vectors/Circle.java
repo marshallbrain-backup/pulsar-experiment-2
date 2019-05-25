c32dd916ce1b1562bf18fc6a24b0ae85940a0787
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-25 10:38:37 -0400

    Initial commit
    A circle vector.

---
+++ src/main/java/com/brain/ion/graphics/vectors/Circle.java
@@ -0,0 +1,10 @@
+package main.java.com.brain.ion.graphics.vectors;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "circle")
+public class Circle implements Vector {
+	
+	
+	
+}
