d16188a2572bbd89c23ffac697a0f9e2c70ac411
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-26 09:20:35 -0400

    Scale the drawn body to the current zoom
    Zoom is handled by a number that represents how many meters the width of
    the screen should represent.

--- src/main/java/com/brain/pulsar/ui/map/BodyUi.java
+++ src/main/java/com/brain/pulsar/ui/map/BodyUi.java
@@ -9,6 +9,8 @@ import java.util.List;
 import main.java.com.brain.ion.graphics.VectorGraphics;
 import main.java.com.brain.ion.graphics.vectors.Vector;
 import main.java.com.brain.ion.graphics.vectors.VectorGroup;
+import main.java.com.brain.pulsar.data.Distance;
+import main.java.com.brain.pulsar.data.DistanceType;
 import main.java.com.brain.pulsar.universe.Body;
 
 public class BodyUi {
@@ -27,8 +29,24 @@ public class BodyUi {
 		
 		List<Vector> vectors = vectorLayer.getVectors();
 		
+		double distanceRatio = g.getWindowWidth() / Distance.convert(5, 0, DistanceType.AU, DistanceType.METER).getDistance();
+		Distance radius = body.getRadius().convert(DistanceType.METER);
+		
+		int x = Math.toIntExact(Math.round(body.getX() * distanceRatio));
+		int y = Math.toIntExact(Math.round(body.getY() * distanceRatio));
+		int r = Math.toIntExact(Math.round(radius.getDistance() * distanceRatio));
+		
+		if(r < 5) {
+			r = 5;
+		}
+		
+		AffineTransform at = new AffineTransform();
+		at.translate(x, y);
+		
 		for(Vector v: vectors) {
-			Shape s = v.getShape(50, 50);
+			
+			Shape s = v.getShape(r, r);
+			s = at.createTransformedShape(s);
 			g.draw(s);
 		}
 		
