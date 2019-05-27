9c7905c3972040eddd0db54c8b512b041a66f295
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-27 13:47:19 -0400

    Fix the xml annotation

--- src/main/java/com/brain/ion/graphics/IonXmlRoot.java
+++ src/main/java/com/brain/ion/graphics/IonXmlRoot.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import main.java.com.brain.ion.graphics.vectors.Vector;
 import main.java.com.brain.ion.graphics.vectors.VectorGroup;
 
+@XmlRootElement(name = "ion")
 public class IonXmlRoot {
 	
-	@XmlElement
+	@XmlElement(name = "vector_layer")
 	private List<VectorGroup> vectors;
 	
 	public List<VectorGroup> getVectorGroups() {
