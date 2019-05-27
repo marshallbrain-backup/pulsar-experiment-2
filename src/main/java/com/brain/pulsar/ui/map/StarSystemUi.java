7168684438b1820eefa6fc33dd1f155898617aa6
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-27 15:57:39 -0400

    Add the ability to move and zoom the system

--- src/main/java/com/brain/pulsar/ui/map/StarSystemUi.java
+++ src/main/java/com/brain/pulsar/ui/map/StarSystemUi.java
@@ -1,6 +1,8 @@
 package main.java.com.brain.pulsar.ui.map;
 
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,13 +18,18 @@ import main.java.com.brain.pulsar.universe.StarSystem;
 
 public class StarSystemUi {
 	
+	private double zoom;
 	private StarSystem starSystem;
 	private List<BodyUi> bodyList;
+	private Point offsetAmount;
 
 	public StarSystemUi(StarSystem mainSystem, Map<String, VectorGroup> bodys) {
 		
+		zoom = 1;
+		
 		starSystem = mainSystem;
 		
+		offsetAmount = new Point(0, 0);
 		bodyList = new ArrayList<>();
 		
 		VectorGroup base = bodys.getOrDefault("", new VectorGroup());
@@ -35,14 +42,35 @@ public class StarSystemUi {
 
 	public void tick(Mouse m) {
 		
+		if(m.buttonDown(1)) {
+			Point d = m.getChange();
+			if(d.getX() != 0 || d.getY() != 0) {
+				offsetAmount.translate(-d.x, -d.y);
+			}
+		}
+		if(m.getWheelDir() != 0) {
+			double newZoom = zoom+m.getWheelDir();
+			if(newZoom == 0) {
+				newZoom += m.getWheelDir();
+			}
+			
+			zoom = newZoom;
+			
+		}
+		
 	}
 
 	public void render(VectorGraphics g) {
 		
 		g.setTranslate(ScreenPosition.CENTER);
+		g.moveTranslate(offsetAmount.getX(), offsetAmount.getY());
+		
+		AffineTransform transform = new AffineTransform();
+		double scale = Math.pow(1.1, zoom);
+		transform.scale(scale, scale);
 		
 		for(BodyUi b: bodyList) {
-			b.render(g);
+			b.render(g, transform);
 		}
 		
 	}
