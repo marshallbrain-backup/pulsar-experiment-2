80389669b727003d563d40aaa3306afc9b033b0c
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-27 13:44:18 -0400

    Display the body's orbit around its parent

--- src/main/java/com/brain/pulsar/ui/map/BodyUi.java
+++ src/main/java/com/brain/pulsar/ui/map/BodyUi.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import main.java.com.brain.ion.graphics.VectorGraphics;
 import main.java.com.brain.ion.graphics.vectors.Vector;
@@ -29,25 +33,41 @@ public class BodyUi {
 		
 		List<Vector> vectors = vectorLayer.getVectors();
 		
-		double distanceRatio = g.getWindowWidth() / Distance.convert(5, 0, DistanceType.AU, DistanceType.METER).getDistance();
+		double distanceRatio = g.getWindowWidth() / Distance.convert(20, 0, DistanceType.AU, DistanceType.METER).getDistance();
 		Distance radius = body.getRadius().convert(DistanceType.METER);
+		Distance distance = body.getDistance().convert(DistanceType.METER);
 		
 		int x = Math.toIntExact(Math.round(body.getX() * distanceRatio));
 		int y = Math.toIntExact(Math.round(body.getY() * distanceRatio));
+		
+		int parX = Math.toIntExact(Math.round(body.getParent().getX() * distanceRatio));
+		int parY = Math.toIntExact(Math.round(body.getParent().getY() * distanceRatio));
+		
 		int r = Math.toIntExact(Math.round(radius.getDistance() * distanceRatio));
+		int d = Math.toIntExact(Math.round(distance.getDistance() * distanceRatio));
 		
 		if(r < 5) {
 			r = 5;
 		}
 		
 		AffineTransform at = new AffineTransform();
-		at.translate(x, y);
 		
+		Shape e = new Ellipse2D.Double(parX-d, parY-d, d*2, d*2);
+		e = at.createTransformedShape(e);
+		
+		Map<String, String> style = new HashMap<>();
+		style.put("stroke", "#000000");
+		style.put("stroke-opacity", "1");
+		style.put("stroke-width", "2");
+		
+		g.draw(e, style);
+		
+		at.translate(x, y);
 		for(Vector v: vectors) {
 			
 			Shape s = v.getShape(r, r);
 			s = at.createTransformedShape(s);
-			g.draw(s);
+			g.draw(s, v.getStyle());
 		}
 		
 	}
