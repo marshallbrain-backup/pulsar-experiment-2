44be06cc9ffab8f4d2473406e3a27a38bc0b3e93
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-28 13:23:13 -0400

    Make the orbit and body line up exactly
    Previously, a body and its orbit did not line up when zoomed in close
    enough. This was most likely caused because a circle drawn using the
    Ellips2D class does not draw an exact circle as dictated by
    trigonometry.
    
    This was fixed by changing the orbit to a arc that starts at the angel
    that the planet is at. The planet is then rendered at the starting point
    of the arc.

--- src/main/java/com/brain/pulsar/ui/map/BodyUi.java
+++ src/main/java/com/brain/pulsar/ui/map/BodyUi.java
@@ -2,13 +2,18 @@ package main.java.com.brain.pulsar.ui.map;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import main.java.com.brain.ion.graphics.VectorGraphics;
 import main.java.com.brain.ion.graphics.vectors.Vector;
@@ -29,45 +34,60 @@ public class BodyUi {
 		
 	}
 
-	public void render(VectorGraphics g, AffineTransform transform) {
+	public void render(VectorGraphics g, double scale) {
 		
 		List<Vector> vectors = vectorLayer.getVectors();
 		
 		double distanceRatio = g.getWindowWidth() / Distance.convert(20, 0, DistanceType.AU, DistanceType.METER).getDistance();
-		Distance radius = body.getRadius().convert(DistanceType.METER);
-		Distance distance = body.getDistance().convert(DistanceType.METER);
+		distanceRatio *= scale;
 		
-		int x = Math.toIntExact(Math.round(body.getX() * distanceRatio));
-		int y = Math.toIntExact(Math.round(body.getY() * distanceRatio));
+		double parentX = body.getParent().getX() * distanceRatio;
+		double parentY = body.getParent().getY() * distanceRatio;
 		
-		int parX = Math.toIntExact(Math.round(body.getParent().getX() * distanceRatio));
-		int parY = Math.toIntExact(Math.round(body.getParent().getY() * distanceRatio));
+		double radius = body.getRadius().convert(DistanceType.METER).getDistance() * distanceRatio;
+		double distance = body.getDistance().convert(DistanceType.METER).getDistance() * distanceRatio;
 		
-		int r = Math.toIntExact(Math.round(radius.getDistance() * distanceRatio));
-		int d = Math.toIntExact(Math.round(distance.getDistance() * distanceRatio));
-		
-		if(r < 5) {
-			r = 5;
+		if(radius < 10) {
+			radius = 10;
 		}
 		
-		AffineTransform at = new AffineTransform(transform);
-		
-		Shape e = new Ellipse2D.Double(parX-d, parY-d, d*2, d*2);
-		e = at.createTransformedShape(e);
+		AffineTransform at = new AffineTransform();
 		
 		Map<String, String> style = new HashMap<>();
 		style.put("stroke", "#000000");
 		style.put("stroke-opacity", "1");
 		style.put("stroke-width", "2");
 		
-		g.draw(e, style);
+		Ellipse2D orbit = new Ellipse2D.Double(parentX-distance, parentY-distance, distance*2, distance*2);
+		Arc2D orbitArc = new Arc2D.Double(orbit.getBounds2D(), body.getAngle(), 360, Arc2D.OPEN);
+		Point2D curveStart = orbitArc.getStartPoint();
 		
-		at.translate(x, y);
+		g.draw(orbitArc, style);
+
+		Map<Shape, Map<String, String>> shapeList = new HashMap<>();
+		double max = 1;
 		for(Vector v: vectors) {
 			
-			Shape s = v.getShape(r, r);
-			s = at.createTransformedShape(s);
-			g.draw(s, v.getStyle());
+			Shape s = v.getShape();
+			shapeList.put(s, v.getStyle());
+			
+			Rectangle2D bounds = s.getBounds2D();
+			if(max < bounds.getWidth()/2) {
+				max = bounds.getWidth()/2;
+			}
+			if(max < bounds.getHeight()/2) {
+				max = bounds.getHeight()/2;
+			}
+			
+		}
+		
+		at.translate(curveStart.getX(), curveStart.getY());
+		at.scale(1/max, 1/max);
+		at.scale(radius, radius);
+		
+		for(Entry<Shape, Map<String, String>> e: shapeList.entrySet()) {
+			Shape s = at.createTransformedShape(e.getKey());
+			g.draw(s, e.getValue());
 		}
 		
 	}
