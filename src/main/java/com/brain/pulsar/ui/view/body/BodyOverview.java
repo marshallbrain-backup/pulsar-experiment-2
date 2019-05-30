bbaa1cf47de1782886cad6f22bbd9d0aa9e0c0d4
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-30 15:15:00 -0400

    Add new constructor

--- src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
+++ src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
@@ -2,8 +2,12 @@ package com.brain.pulsar.ui.view.body;
 
 import com.brain.ion.graphics.VectorGraphics;
 import com.brain.pulsar.ui.view.View;
+import com.brain.pulsar.universe.Body;
 
 public class BodyOverview implements View {
+
+	public BodyOverview(Body body) {
+	}
 
 	@Override
 	public void tick() {
