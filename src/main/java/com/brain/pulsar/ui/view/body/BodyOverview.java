0d7cfec99b7c0131b04ab43a63f0fd2c39355ceb
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-30 15:54:36 -0400

    Render vectorGroups

--- src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
+++ src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
@@ -3,13 +3,23 @@ package com.brain.pulsar.ui.view.body;
 import java.util.Map;
 
 import com.brain.ion.graphics.VectorGraphics;
+import com.brain.ion.graphics.vectors.Vector;
 import com.brain.ion.graphics.vectors.VectorGroup;
 import com.brain.pulsar.ui.view.View;
 import com.brain.pulsar.universe.Body;
 
 public class BodyOverview implements View {
+	
+	private int windowCode;
+	
+	private Body body;
+	private Map<String, VectorGroup> vectorGroups;
 
-	public BodyOverview(Body body, Map<String, VectorGroup> view) {
+	public BodyOverview(Body body, Map<String, VectorGroup> vectorGroups) {
+		
+		this.body = body;
+		this.vectorGroups = vectorGroups;
+		
 	}
 
 	@Override
@@ -20,6 +30,19 @@ public class BodyOverview implements View {
 	@Override
 	public void render(VectorGraphics g) {
 		
+		windowCode = g.getParentCode();
+		
+		VectorGroup frame = vectorGroups.get("frame");
+		
+		for(Vector v: frame.getVectors()) {
+			g.draw(v.getShape(), v.getStyle());
+		}
+		
+	}
+
+	@Override
+	public int getWindowCode() {
+		return windowCode;
 	}
 	
 }
