d83e4857191e74c8aab312afc00326da3fadf714
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-30 14:08:45 -0400

    Initial commit
    View for viewing body information.

---
+++ src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
@@ -0,0 +1,18 @@
+package com.brain.pulsar.ui.view.body;
+
+import com.brain.ion.graphics.VectorGraphics;
+import com.brain.pulsar.ui.view.View;
+
+public class BodyOverview implements View {
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(VectorGraphics g) {
+		
+	}
+	
+}
