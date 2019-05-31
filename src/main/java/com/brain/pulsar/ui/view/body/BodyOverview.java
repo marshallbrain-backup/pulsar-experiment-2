1c3cd8f8cb5c401a1fdd04ea5f4cb4fa73beb11b
MarshallDBrain <marshalldbrain@gmail.com>
2019-05-31 00:53:57 -0400

    Add rendering of basic info

--- src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
+++ src/main/java/com/brain/pulsar/ui/view/body/BodyOverview.java
@@ -1,10 +1,13 @@
 package com.brain.pulsar.ui.view.body;
 
+import java.awt.Shape;
+import java.awt.geom.Area;
 import java.util.Map;
 
 import com.brain.ion.graphics.VectorGraphics;
 import com.brain.ion.graphics.vectors.Vector;
 import com.brain.ion.graphics.vectors.VectorGroup;
+import com.brain.ion.input.Mouse;
 import com.brain.pulsar.ui.view.View;
 import com.brain.pulsar.universe.Body;
 
@@ -13,6 +16,7 @@ public class BodyOverview implements View {
 	private int windowCode;
 	
 	private Body body;
+	private Area lastRendered;
 	private Map<String, VectorGroup> vectorGroups;
 
 	public BodyOverview(Body body, Map<String, VectorGroup> vectorGroups) {
@@ -20,31 +24,49 @@ public class BodyOverview implements View {
 		this.body = body;
 		this.vectorGroups = vectorGroups;
 		
+		lastRendered = new Area();
+		
 	}
 
 	@Override
-	public void tick() {
+	public boolean tick(Mouse m) {
+		
+		if(lastRendered.contains(m.getPosition())) {
+			return true;
+		}
+		
+		return false;
 		
 	}
 
 	@Override
 	public void render(VectorGraphics g) {
 		
+		lastRendered.reset();
 		windowCode = g.getParentCode();
-		
 		g.moveTranslate(150, 100);
 		
 		VectorGroup frame = vectorGroups.get("frame");
+		VectorGroup header = vectorGroups.get("header");
+		VectorGroup info = vectorGroups.get("basic_info");
+		
+		g.beginAreaRendering();
 		
 		for(Vector v: frame.getVectors()) {
 			g.draw(v.getShape(), v.getStyle());
 		}
 		
-		VectorGroup header = vectorGroups.get("header");
-		
 		for(Vector v: header.getVectors()) {
 			g.draw(v.getShape(), v.getStyle());
 		}
+		
+		g.moveTranslate(info.getOrigin());
+		
+		for(Vector v: info.getVectors()) {
+			g.draw(v.getShape(g.getGraphics()), v.getStyle());
+		}
+		
+		lastRendered.add(g.resetAreaRendering());
 		
 	}
 	
