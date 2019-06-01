e95f1852e1ced6adfd4e97bb0797386ab09d2017
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-01 17:28:22 -0400

    Add check for if the district is valid for the given scope

--- src/main/java/com/brain/pulsar/xml/types/DistrictType.java
+++ src/main/java/com/brain/pulsar/xml/types/DistrictType.java
@@ -1,26 +1,61 @@
 package com.brain.pulsar.xml.types;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import com.brain.ion.xml.StringTrimAdapter;
+import com.brain.pulsar.other.Scope;
+import com.brain.pulsar.xml.elements.Trigger;
 
 @XmlRootElement(name = "district")
 public class DistrictType {
 	
 	@XmlElement(name = "base_buildtime")
 	private int baseBuildtime;
+	
+	@XmlElementWrapper(name="potential")
+	@XmlElement(name = "trigger")
+	private List<Trigger> potential;
+
+	public boolean isPotential(Scope bodyType) {
+		
+		for(Trigger t: potential) {
+			if(t.isCondition(bodyType)) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
 
 	@Override
 	public boolean equals(Object obj) {
 		
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (!(obj instanceof DistrictType)) {
 			return false;
+		}
 		DistrictType other = (DistrictType) obj;
-		if (baseBuildtime != other.baseBuildtime)
+		if (baseBuildtime != other.baseBuildtime) {
 			return false;
+		}
+		if (potential == null) {
+			if (other.potential != null) {
+				return false;
+			}
+		} else if (!potential.equals(other.potential)) {
+			return false;
+		}
 		return true;
 	}
 
