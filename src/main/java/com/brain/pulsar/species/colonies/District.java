d8102a6e6b495c379f4f3b68ec6dfdc83694b086
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-01 17:29:15 -0400

    Add district creation

--- src/main/java/com/brain/pulsar/species/colonies/District.java
+++ src/main/java/com/brain/pulsar/species/colonies/District.java
@@ -1,6 +1,24 @@
 package com.brain.pulsar.species.colonies;
 
+import com.brain.pulsar.other.Scope;
+import com.brain.pulsar.universe.Body;
+import com.brain.pulsar.xml.types.DistrictType;
 
 public class District {
+	
+	private DistrictType type;
+
+	private District(DistrictType type) {
+		this.type = type;
+	}
+
+	public static District create(DistrictType districtType, Body body) {
+		
+		if(districtType.isPotential(new Scope(body.getType()))) {
+			return new District(districtType);
+		}
+		
+		return null;
+	}
 	
 }
