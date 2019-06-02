b3911376a05825f7157f4df9fc4be18641c42aa9
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-02 18:10:32 -0400

    Initial commit
    Used for determining if an object has an income.

---
+++ src/main/java/com/brain/pulsar/species/colonies/Transactional.java
@@ -0,0 +1,11 @@
+package com.brain.pulsar.species.colonies;
+
+import java.util.List;
+
+import com.brain.pulsar.xml.elements.Resource;
+
+public interface Transactional {
+
+	 List<Resource> getIncome();
+	
+}
