968155611abfbdda1609e0d35085989972ddced5
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-02 18:11:31 -0400

    Add all the methods

--- src/main/java/com/brain/pulsar/xml/elements/Operations.java
+++ src/main/java/com/brain/pulsar/xml/elements/Operations.java
@@ -1,5 +1,8 @@
 package com.brain.pulsar.xml.elements;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -16,5 +19,52 @@ public class Operations {
 	@XmlElementWrapper
 	@XmlElement(name = "resource")
 	private List<Resource> production;
+	
+	public Operations() {
+	}
+	
+	public Operations(Operations clone) {
+		
+		cost = new ArrayList<>(clone.cost);
+		upkeep = new ArrayList<>(clone.upkeep);
+		production = new ArrayList<>(clone.production);
+		
+	}
+
+	public static List<Operations> toList(Operations... ops) {
+		
+		return toList(ops, new Operations[0]);
+	}
+	
+	public static List<Operations> toList(Operations[]... opsArray) {
+		
+		List<Operations> ops = new ArrayList<>();
+		
+		for(Operations[] o: opsArray) {
+			ops.addAll(Arrays.asList(o));
+		}
+		
+		return ops;
+	}
+
+	public List<Resource> getUpkeep() {
+		
+		List<Resource> newUpkeep = new ArrayList<>();
+		for(Resource r: upkeep) {
+			newUpkeep.add(new Resource(-r.getAmount(), r.getType()));
+		}
+		
+		return newUpkeep;
+	}
+
+	public List<Resource> getProduction() {
+		
+		List<Resource> newProduction = new ArrayList<>();
+		for(Resource r: production) {
+			newProduction.add(new Resource(r.getAmount(), r.getType()));
+		}
+		
+		return newProduction;
+	}
 	
 }
--- src/main/java/com/brain/pulsar/xml/elements/Resource.java
+++ src/main/java/com/brain/pulsar/xml/elements/Resource.java
@@ -6,9 +6,103 @@ import javax.xml.bind.annotation.XmlValue;
 public class Resource {
 	
 	@XmlValue
-	private double amount;
+	private final double amount;
 
 	@XmlAttribute
-	private String type;
+	private final String type;
+	
+	public Resource() {
+		amount = 0;
+		type = null;
+	}
+	
+	public Resource(double amount, String type) {
+		
+		this.amount = amount;
+		this.type = type;
+	}
+
+	public static Resource append(Resource res1, Resource res2) {
+		
+		if(!res1.equals(res2)) {
+			return null;
+		}
+		
+		double amount = res1.amount + res2.amount;
+		String type = res1.type;
+		
+		return new Resource(amount, type);
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	@Override
+	public int hashCode() {
+		
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Resource)) {
+			return false;
+		}
+		Resource other = (Resource) obj;
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!type.equals(other.type)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean equalsExact(Object obj) {
+		
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Resource)) {
+			return false;
+		}
+		Resource other = (Resource) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount)) {
+			return false;
+		}
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!type.equals(other.type)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Resource [amount=" + amount + ", type=" + type + "]";
+	}
 	
 }
