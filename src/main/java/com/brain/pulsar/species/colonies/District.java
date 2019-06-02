b3e84f9cfabdb630e55d332459f1f40d5d7cdab8
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-02 18:13:04 -0400

    Add the getIncome method

--- src/main/java/com/brain/pulsar/species/colonies/District.java
+++ src/main/java/com/brain/pulsar/species/colonies/District.java
@@ -1,16 +1,17 @@
 package com.brain.pulsar.species.colonies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.brain.pulsar.other.Scope;
 import com.brain.pulsar.universe.Body;
+import com.brain.pulsar.xml.elements.Operations;
+import com.brain.pulsar.xml.elements.Resource;
 import com.brain.pulsar.xml.types.DistrictType;
 
-public class District {
+public class District implements Transactional {
 	
 	private DistrictType type;
-
-	private District(DistrictType type) {
-		this.type = type;
-	}
 
 	public static District create(DistrictType districtType, Body body) {
 		
@@ -20,5 +21,29 @@ public class District {
 		
 		return null;
 	}
+
+	private District(DistrictType type) {
+		this.type = type;
+	}
+
+	public boolean isType(DistrictType t) {
+		
+		return type.equals(t);
+	}
+
+	@Override
+	public List<Resource> getIncome() {
+		
+		List<Resource> income = new ArrayList<>();
+		
+		Operations ops = type.getOperations();
+		
+		income.addAll(ops.getUpkeep());
+		income.addAll(ops.getProduction());
+		
+		return income;
+	}
+	
+	
 	
 }
