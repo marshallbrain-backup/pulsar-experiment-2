73fc7afd8e84a6fa3d2151d8ef0e8df532dc392c
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-04 10:45:57 -0400

    Refactor code

--- src/main/java/com/brain/pulsar/species/colonies/District.java
+++ src/main/java/com/brain/pulsar/species/colonies/District.java
@@ -1,18 +1,13 @@
 package com.brain.pulsar.species.colonies;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.brain.pulsar.other.Scope;
+import com.brain.pulsar.species.ResourceBucket;
 import com.brain.pulsar.universe.Body;
-import com.brain.pulsar.xml.elements.Operations;
-import com.brain.pulsar.xml.elements.Resource;
 import com.brain.pulsar.xml.types.DistrictType;
 
-public class District implements Transactional, Constructible {
+public class District {
 	
 	private int amount;
-	private int queuedAmount;
 	
 	private DistrictType type;
 
@@ -28,60 +23,11 @@ public class District implements Transactional, Constructible {
 	private District(DistrictType type) {
 		this.type = type;
 	}
-
-	public boolean isType(DistrictType t) {
-		
-		return type.equals(t);
-	}
-
-	@Override
-	public List<Resource> getIncome() {
-		
-		List<Resource> income = new ArrayList<>();
-		
-		Operations ops = type.getOperations();
-		
-		income.addAll(ops.getUpkeep());
-		income.addAll(ops.getProduction());
-		
-		return income;
-	}
-
-	public int getQueuedAmount() {
-		
-		return queuedAmount;
-	}
-
-	public int getAmount() {
-		
-		return amount;
-	}
-
-	@Override
-	public List<Resource> getBuildCost() {
-		
-		Operations ops = type.getOperations();
-		
-		return new ArrayList<>(ops.getCost());
-	}
-
-	@Override
-	public void queueBuild() {
-		
-		queuedAmount++;
-		
-	}
-
-	@Override
-	public void finnishBuild() {
-		
-		if(queuedAmount > 0) {
-			amount++;
-			queuedAmount--;
-		}
-		
-	}
 	
-	
+	public ResourceBucket getResources() {
+		
+		return new ResourceBucket(type.getId(), type.getOperations().getUpkeep(), type.getOperations().getProduction());
+		
+	}
 	
 }
