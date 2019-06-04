007b181139277d545e0faef5922279f1d6ac1c2a
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-04 10:43:43 -0400

    Remove cost list

--- src/main/java/com/brain/pulsar/xml/elements/Operations.java
+++ src/main/java/com/brain/pulsar/xml/elements/Operations.java
@@ -12,9 +12,6 @@ public class Operations {
 	
 	@XmlElementWrapper
 	@XmlElement(name = "resource")
-	private List<Resource> cost;
-	@XmlElementWrapper
-	@XmlElement(name = "resource")
 	private List<Resource> upkeep;
 	@XmlElementWrapper
 	@XmlElement(name = "resource")
@@ -25,49 +22,17 @@ public class Operations {
 	
 	public Operations(Operations clone) {
 		
-		cost = new ArrayList<>(clone.cost);
 		upkeep = new ArrayList<>(clone.upkeep);
 		production = new ArrayList<>(clone.production);
 		
 	}
 
-	public static List<Operations> toList(Operations... ops) {
-		
-		return toList(ops, new Operations[0]);
-	}
-	
-	public static List<Operations> toList(Operations[]... opsArray) {
-		
-		List<Operations> ops = new ArrayList<>();
-		
-		for(Operations[] o: opsArray) {
-			ops.addAll(Arrays.asList(o));
-		}
-		
-		return ops;
-	}
-
 	public List<Resource> getUpkeep() {
-		return cloneList(upkeep);
+		return new ArrayList<>(upkeep);
 	}
 
 	public List<Resource> getProduction() {
-		return cloneList(production);
-	}
-
-	public List<Resource> getCost() {
-		return cloneList(cost);
-	}
-	
-	private static List<Resource> cloneList(List<Resource> list) {
-		
-		List<Resource> newList = new ArrayList<>();
-		for(Resource r: list) {
-			newList.add(new Resource(r.getAmount(), r.getType()));
-		}
-		
-		return newList;
-		
+		return new ArrayList<>(production);
 	}
 	
 }
