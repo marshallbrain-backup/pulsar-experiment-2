a6b85408fab048803ca6fd0906733695f42a091e
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-22 16:43:56 -0400

    Initial commit
    Keeps track of resources and its child buckets.

---
+++ src/main/java/com/brain/pulsar/buckets/ResourceBucket.java
@@ -0,0 +1,63 @@
+package com.brain.pulsar.buckets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.brain.pulsar.xml.elements.Resource;
+
+public class ResourceBucket {
+	
+	private final String name;
+	private final String type;
+	
+	private List<ResourceBucket> buckets;
+	private List<Resource> resources;
+
+	public ResourceBucket(String type, String name) {
+		
+		this.name = name;
+		this.type = type;
+		
+		buckets = new ArrayList<>();
+		resources = new ArrayList<>();
+		
+	}
+
+	public void addBucket(ResourceBucket bucket) {
+		
+		buckets.add(bucket);
+	}
+
+	public void merge() {
+		
+		for(ResourceBucket b: buckets) {
+			
+			b.merge();
+			
+			for(Resource r: b.getResources()) {
+				
+				if(resources.contains(r)) {
+					int i = resources.indexOf(r);
+					r.combine(resources.set(i, r));
+					
+				} else {
+					resources.add(r);
+				}
+				
+			}
+			
+		}
+		
+	}
+
+	public void addResource(List<Resource> list) {
+		
+		resources.addAll(list);
+	}
+
+	public List<Resource> getResources() {
+		
+		return resources;
+	}
+	
+}
