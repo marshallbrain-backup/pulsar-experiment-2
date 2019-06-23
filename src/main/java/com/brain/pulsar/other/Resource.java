6c334bb7a8fa7cf46c8ec458d9e048d27b3dc9f7
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-23 14:02:00 -0400

    Add chain lists
    Keeps track of where each resource is coming from.

--- src/main/java/com/brain/pulsar/other/Resource.java
+++ src/main/java/com/brain/pulsar/other/Resource.java
@@ -1,5 +1,8 @@
 package com.brain.pulsar.other;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
@@ -7,43 +10,82 @@ import com.brain.pulsar.xml.elements.ResourceBase;
 
 public class Resource {
 	
-	@XmlValue
 	private double amount;
 	
-	@XmlAttribute
-	private final String id;
+	private String id;
+	
+	private List<String> idChain;
+	private List<String> typeChain;
 	
 	public Resource() {
-		id = null;
+		
+		idChain = new ArrayList<>();
+		typeChain = new ArrayList<>();
 	}
 
 	public Resource(String id, double amount) {
+		
+		this();
+		
 		this.id = id;
 		this.amount = amount;
 	}
 
 	public Resource(ResourceBase r) {
+		
+		this();
+		
 		id = r.getId();
 		amount = r.getAmount();
 	}
 
 	public Resource(ResourceBase r, boolean b) {
+		
+		this();
+		
 		id = r.getId();
+		
 		if(b) {
 			amount = r.getAmount() * -1;
 		} else {
 			amount = r.getAmount();
 		}
 	}
+	
+	private Resource(Resource r, List<String> typeChain, List<String> idChain) {
+		
+		amount = r.amount;
+		id = r.id;
+		this.typeChain = new ArrayList<>(typeChain);
+		this.idChain = new ArrayList<>(idChain);
+		
+	}
 
 	public void combine(Resource r) {
 		
 		if(equals(r)) {
-			
 			amount += r.amount;
-			
 		}
+	}
+
+	public void appendChain(String type, String name) {
+
+		typeChain.add(type);
+		idChain.add(name);
+	}
+
+	public Resource trim() {
 		
+		List<String> newIdChain = new ArrayList<>(idChain.subList(1, idChain.size()));
+		List<String> newTypeChain = new ArrayList<>(typeChain.subList(1, typeChain.size()));
+		
+		return new Resource(this, newTypeChain, newIdChain);
+		
+	}
+
+	public boolean isChainEmpty() {
+		
+		return typeChain.isEmpty() && idChain.isEmpty();
 	}
 
 	@Override
@@ -52,6 +94,8 @@ public class Resource {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idChain == null) ? 0 : idChain.hashCode());
+		result = prime * result + ((typeChain == null) ? 0 : typeChain.hashCode());
 		return result;
 	}
 
@@ -73,6 +117,20 @@ public class Resource {
 				return false;
 			}
 		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (idChain == null) {
+			if (other.idChain != null) {
+				return false;
+			}
+		} else if (!idChain.equals(other.idChain)) {
+			return false;
+		}
+		if (typeChain == null) {
+			if (other.typeChain != null) {
+				return false;
+			}
+		} else if (!typeChain.equals(other.typeChain)) {
 			return false;
 		}
 		return true;
