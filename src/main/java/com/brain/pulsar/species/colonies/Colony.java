1385876c454a30083be1b9ef5f2d561ab2ec200b
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-27 15:58:50 -0400

    Add buildings and construction

--- src/main/java/com/brain/pulsar/species/colonies/Colony.java
+++ src/main/java/com/brain/pulsar/species/colonies/Colony.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.brain.pulsar.universe.Body;
+import com.brain.pulsar.xml.types.BuildingType;
 import com.brain.pulsar.xml.types.DistrictType;
 
 public class Colony {
@@ -13,17 +14,27 @@ public class Colony {
 	private District[] districts;
 	
 	private final Body parent;
-	
+
+	private final List<Building> buildings;
 	private final List<DistrictType> districtTypes;
+	private final List<BuildingType> buildingTypes;
 	
-	public Colony(Body b, List<DistrictType> dt) {
+	private final ConstructionCoordinator constructionManager;
+	
+	public Colony(Body b, List<DistrictType> dt, List<BuildingType> bt) {
 		
 		parent = b;
 		
 		districts = new District[4];
+		buildings = new ArrayList<>();
 		districtTypes = Collections.unmodifiableList(dt);
+		buildingTypes = Collections.unmodifiableList(bt);
+		constructionManager = new ConstructionCoordinator();
 		
 		initDistricts();
+		initBuildings();
+		
+		constructionManager.flush();
 	}
 
 	private void initDistricts() {
@@ -34,7 +45,24 @@ public class Colony {
 		
 		int i = 0;
 		for(DistrictType dt: districtTypes) {
-			if(districts[i].setDistrictType(dt, parent)) {
+			if(districts[i].setRetoolingType(dt, parent)) {
+				constructionManager.add(districts[i]);
+				i++;
+			}
+		}
+		
+	}
+
+	private void initBuildings() {
+		
+		for(int i = 0; i < 10; i++) {
+			buildings.add(new Building());
+		}
+		
+		int i = 0;
+		for(BuildingType bt: buildingTypes) {
+			if(buildings.get(i).setPendingType(bt, parent)) {
+				constructionManager.add(buildings.get(i));
 				i++;
 			}
 		}
@@ -43,6 +71,11 @@ public class Colony {
 	
 	public List<District> getDistricts(){
 		return Arrays.asList(districts);
+	}
+
+	public List<Building> getBuildings() {
+		
+		return new ArrayList<>(buildings);
 	}
 	
 }
