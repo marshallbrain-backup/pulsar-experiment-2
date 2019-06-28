fed7c4dd98493e286135bb03146f0a70eb8384fe
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-28 09:04:37 -0400

    Add pop interaction with jobs

--- src/main/java/com/brain/pulsar/other/JobCollection.java
+++ src/main/java/com/brain/pulsar/other/JobCollection.java
@@ -7,66 +7,98 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.brain.pulsar.xml.elements.Modifier;
 
 public class JobCollection implements ResourceManager {
 	
-	private Map<String, Map<String, List<Job>>> jobs;
+	private Map<String, List<Job>> jobs;
+	
+	private List<Pop> pops;
 
-	public JobCollection() {
+	public JobCollection(List<Pop> pops) {
+		
+		this.pops = pops;
 		
 		jobs = new HashMap<>();
 	}
 
-	private void addJobs(List<Job> list) {
+	public void tick() {
 		
-		for(Job j: list) {
+		List<Pop> unimployed = pops.stream().filter(Pop::isUnimployed).collect(Collectors.toList());
+		
+		if(!unimployed.isEmpty()) {
 			
-			Map<String, List<Job>> type = new HashMap<>();
-			Map<String, List<Job>> temp1 = jobs.putIfAbsent(j.getType(), type);
+			int i = 0;
 			
-			if(temp1 != null) {
-				type = temp1;
+			for(List<Job> l: jobs.values()) {
+				
+				for(Job j: l) {
+				
+					if(!j.hasPop()) {
+						j.setPop(unimployed.get(i));
+						i++;
+					}
+					
+					if(i >= unimployed.size()) {
+						break;
+					}
+					
+				}
+				
+				if(i >= unimployed.size()) {
+					break;
+				}
+				
 			}
 			
-			List<Job> ids = new ArrayList<>();
-			List<Job> temp2 = type.putIfAbsent(j.getName(), ids);
+		}
+		
+	}
+
+	private void addJobs(Job... jobArray) {
+		
+		for(Job j: jobArray) {
 			
-			if(temp2 != null) {
-				ids = temp2;
+			List<Job> list = jobs.get(j.getKey());
+			if(list == null) {
+				list = new ArrayList<>();
+				jobs.put(j.getKey(), list);
 			}
 			
-			ids.add(j);
+			list.add(j);
 			
 		}
 	}
 
 	public void addJobs(JobManager manager) {
-		addJobs(manager.getJobs());
+		addJobs(manager.getJobs().toArray(new Job[0]));
 	}
 
 	@Override
 	public void applyModifiers(String chain, boolean match, Modifier modifier) {
 		
-		for(Job j: jobs.values().stream()
-				.flatMap(order -> order.values().stream())
-				.flatMap(List::stream)
-				.toArray(Job[]::new)) {
+
+		
+		for(List<Job> l: jobs.values()) {
+			for(Job j: l) {
 			
-			StringBuilder bld = new StringBuilder(chain);
-			
-			if(!chain.isEmpty()) {
+				StringBuilder bld = new StringBuilder(chain);
+				
+				if(!chain.isEmpty()) {
+					bld.append(".");
+				}
+				bld.append(j.getType());
 				bld.append(".");
+				bld.append(j.getName());
+				String newChain = bld.toString();
+				
+				boolean newMatch = modifierMatcher(newChain, modifier.getParent(), match);
+				
+				j.applyModifiers(newChain, newMatch, modifier);
+			
 			}
-			bld.append(j.getType());
-			bld.append(".");
-			bld.append(j.getName());
-			String newChain = bld.toString();
-			
-			boolean newMatch = modifierMatcher(newChain, modifier.getParent(), match);
-			
-			j.applyModifiers(newChain, newMatch, modifier);
 			
 		}
 		
@@ -89,18 +121,23 @@ public class JobCollection implements ResourceManager {
 		
 		List<Resource> res = new ArrayList<>();
 		
-		for(Job j: jobs.values().stream()
-				.flatMap(order -> order.values().stream())
-				.flatMap(List::stream)
-				.toArray(Job[]::new)) {
+		for(List<Job> l: jobs.values()) {
+			for(Job j: l) {
 			
-			for(Resource r: j.getResources()) {
-				res.add(r);
+				for(Resource r: j.getResources()) {
+					res.add(r);
+				}
+			
 			}
 			
 		}
 		
 		return res;
+	}
+
+	public Map<String, List<Job>> getJobs() {
+		
+		return jobs;
 	}
 	
 }
