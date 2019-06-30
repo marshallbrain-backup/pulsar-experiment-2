4156514f3cddb4cabb0eb91540d2d8c2cc86b70d
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-30 09:52:01 -0400

    Initial commit
    Renders ui related to districts.

---
+++ src/main/java/com/brain/pulsar/ui/view/body/overview/Districts.java
@@ -0,0 +1,17 @@
+package com.brain.pulsar.ui.view.body.overview;
+
+import java.util.List;
+import java.util.Map;
+
+import com.brain.ion.graphics.VectorGraphics;
+import com.brain.ion.graphics.vectors.VectorGroup;
+import com.brain.pulsar.empires.colonies.Colony;
+import com.brain.pulsar.ui.view.RenderEntry;
+
+public class Districts {
+	
+	public static void render(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
+		
+	}
+	
+}
