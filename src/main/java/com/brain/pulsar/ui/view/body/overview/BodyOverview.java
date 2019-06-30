fe1b351dffc97cc124c0bc699436a34cffbc0387
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-30 09:51:32 -0400

    Move rendering methods
    Makes this class less cluttered.

--- src/main/java/com/brain/pulsar/ui/view/body/overview/BodyOverview.java
+++ src/main/java/com/brain/pulsar/ui/view/body/overview/BodyOverview.java
@@ -16,6 +16,7 @@ import com.brain.ion.graphics.vectors.Text;
 import com.brain.ion.graphics.vectors.Vector;
 import com.brain.ion.graphics.vectors.VectorGroup;
 import com.brain.ion.input.Mouse;
+import com.brain.pulsar.ui.view.RenderEntry;
 import com.brain.pulsar.ui.view.View;
 import com.brain.pulsar.universe.Body;
 
@@ -55,7 +56,7 @@ public class BodyOverview implements View {
 			return;
 		}
 		
-		List<renderEntry> shapes = new ArrayList<>();
+		List<RenderEntry> shapes = new ArrayList<>();
 		
 		lastRendered.reset();
 		windowCode = g.getParentCode();
@@ -74,97 +75,23 @@ public class BodyOverview implements View {
 			g.draw(v.getShape(), v.getStyle());
 		}
 		
-		renderBasicInfo(g, vectorGroups, shapes);
-		renderBasicColonyInfo(g, vectorGroups, shapes);
+		Information.render(g, vectorGroups, shapes, body);
+		Districts.render(g, vectorGroups, shapes, body.getColony());
 		
 		g.setTranslate(ScreenPosition.ZERO);
-		for(renderEntry e: shapes) {
+		for(RenderEntry e: shapes) {
 			g.draw(e.shape, e.style);
 		}
+		
 		lastRendered.add(g.resetAreaRendering());
 		
 	}
 	
-	private void renderBasicInfo(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<renderEntry> shapes) {
-		
-		VectorGroup info = vectorGroups.get("basic_info");
-
-		g.setTranslate(ScreenPosition.ZERO);
-		g.moveTranslate(150, 100);
-		g.moveTranslate(info.getOrigin());
-		
-		Vector infoFrame = info.getVectorById("frame");
-		Vector planetType = ((Text)info.getVectorById("planet_type")).setText(body.getName());
-		
-		Vector[] infoShapeList = new Vector[] {planetType};
-		
-		scaleVector(g, shapes, infoShapeList, infoFrame);
-		
-	}
 	
-	private void renderBasicColonyInfo(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<renderEntry> shapes) {
-		
-		VectorGroup info = vectorGroups.get("basic_info_colony");
-
-		g.setTranslate(ScreenPosition.ZERO);
-		g.moveTranslate(150, 100);
-		g.moveTranslate(info.getOrigin());
-		
-		Vector infoFrame = info.getVectorById("frame");
-		Vector planetType = ((Text)info.getVectorById("colony_type")).setText(body.getColony().getDesignation());
-		
-		Vector[] infoShapeList = new Vector[] {planetType};
-		
-		scaleVector(g, shapes, infoShapeList, infoFrame);
-		
-	}
-	
-	private void scaleVector(VectorGraphics g, List<renderEntry> shapes, Vector[] infoShapeList, Vector infoFrame) {
-		
-		double max = 0;
-		for(Vector v: infoShapeList) {
-			
-			Shape s = v.getShape(g.getGraphics());
-			
-			Rectangle2D b = s.getBounds2D();
-			double w = b.getX() + b.getWidth();
-			if(v instanceof Text) {
-				w += ((Text) v).getPadding().getX();
-			}
-			if(max < w) {
-				max = w;
-			}
-			
-			s = g.getAffineTransform().createTransformedShape(s);
-			shapes.add(new renderEntry(s, v.getStyle()));
-			
-		}
-		
-		Shape infoFrameShape = infoFrame.getShape();
-		
-		AffineTransform infoFrameAT = new AffineTransform(g.getAffineTransform());
-		infoFrameAT.scale(1/infoFrameShape.getBounds2D().getWidth(), 1);
-		infoFrameAT.scale(max, 1);
-		
-		shapes.add(new renderEntry(infoFrameAT.createTransformedShape(infoFrameShape), infoFrame.getStyle()));
-		
-	}
 	
 	@Override
 	public int getWindowCode() {
 		return windowCode;
-	}
-	
-}
-
-class renderEntry {
-	
-	public final Shape shape;
-	public final Map<String, String> style;
-	
-	public renderEntry(Shape shape, Map<String, String> style) {
-		this.shape = shape;
-		this.style = style;
 	}
 	
 }
