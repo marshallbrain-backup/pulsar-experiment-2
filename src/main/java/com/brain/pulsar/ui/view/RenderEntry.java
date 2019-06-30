f1d4aa0991fe9cecfa6ec13e82d5029b3e1e4250
MarshallDBrain <marshalldbrain@gmail.com>
2019-06-30 14:16:26 -0400

    Add image support

--- src/main/java/com/brain/pulsar/ui/view/RenderEntry.java
+++ src/main/java/com/brain/pulsar/ui/view/RenderEntry.java
@@ -3,14 +3,26 @@ package com.brain.pulsar.ui.view;
 import java.awt.Shape;
 import java.util.Map;
 
+import com.brain.ion.graphics.vectors.Image;
+import com.brain.ion.graphics.vectors.Vector;
+
 public class RenderEntry {
 	
 	public final Shape shape;
 	public final Map<String, String> style;
 	
+	public final Image image;
+	
 	public RenderEntry(Shape shape, Map<String, String> style) {
 		this.shape = shape;
 		this.style = style;
+		this.image = null;
+	}
+
+	public RenderEntry(Vector icon) {
+		this.shape = null;
+		this.style = null;
+		this.image = (Image) icon;
 	}
 	
 }
