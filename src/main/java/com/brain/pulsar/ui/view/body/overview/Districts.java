10f5b560c2f3f497300cbd852b2978eb1176a43b
MarshallDBrain <marshalldbrain@gmail.com>
2019-09-05 17:06:42 -0400

    Shows entries for all districts on a colony
    Will get change to be one entry that is cloned in the future.

--- src/main/java/com/brain/pulsar/ui/view/body/overview/Districts.java
+++ src/main/java/com/brain/pulsar/ui/view/body/overview/Districts.java
@@ -7,9 +7,11 @@ import java.util.Map;
 import com.brain.ion.graphics.ScreenPosition;
 import com.brain.ion.graphics.VectorGraphics;
 import com.brain.ion.graphics.vectors.Image;
+import com.brain.ion.graphics.vectors.Text;
 import com.brain.ion.graphics.vectors.Vector;
 import com.brain.ion.graphics.vectors.VectorGroup;
 import com.brain.pulsar.empires.colonies.Colony;
+import com.brain.pulsar.empires.colonies.District;
 import com.brain.pulsar.ui.view.RenderEntry;
 
 public class Districts {
@@ -55,26 +57,35 @@ public class Districts {
 	
 	public static void entry(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
 		
-		VectorGroup info = vectorGroups.get("districts_entry");
+		List<District> districts = colony.getDistricts();
+		
+		for(int i = 0; i < districts.size(); i++) {
+			
+			VectorGroup info = vectorGroups.get("districts_entry_" + i);
 
-		g.setTranslate(ScreenPosition.ZERO);
-		g.moveTranslate(150, 100);
-		g.moveTranslate(info.getOrigin());
-		
-		Vector frame = info.getVectorById("frame");
-		Shape f = frame.getShape();
-		f = g.getAffineTransform().createTransformedShape(f);
-		
-		Vector icon = info.getVectorById("icon");
-		((Image) icon).setXform(g.getAffineTransform());
-		
-		Vector text = info.getVectorById("text");
-		Shape t = text.getShape(g.getGraphics());
-		t = g.getAffineTransform().createTransformedShape(t);
-		
-		shapes.add(new RenderEntry(icon));
-		shapes.add(new RenderEntry(f, frame.getStyle()));
-		shapes.add(new RenderEntry(t, text.getStyle()));
+			g.setTranslate(ScreenPosition.ZERO);
+			g.moveTranslate(150, 100 + i*55);
+			g.moveTranslate(info.getOrigin());
+			
+			Vector frame = info.getVectorById("frame");
+			Shape f = frame.getShape();
+			f = g.getAffineTransform().createTransformedShape(f);
+			
+			Vector icon = info.getVectorById("icon");
+			((Image) icon).setXform(g.getAffineTransform());
+			
+			Text text = (Text) info.getVectorById("amount");
+			text = text.setText(
+					String.format("%02d", districts.get(i).getAmount()) + "/" + 
+					String.format("%02d", districts.get(i).getAmount() + colony.getRemainingDistricts()));
+			Shape t = text.getShape(g.getGraphics());
+			t = g.getAffineTransform().createTransformedShape(t);
+				
+			shapes.add(new RenderEntry(icon));
+			shapes.add(new RenderEntry(f, frame.getStyle()));
+			shapes.add(new RenderEntry(t, text.getStyle()));
+			
+		}
 		
 	}
 	
