621809b3d38231a5707a3c6e5c0034891be5e792
Marshall Brain <Marshall Brain@DESKTOP-7SIC72I>
2019-09-09 14:15:37 -0400

    Fixed class name error

--- src/main/java/com/brain/pulsar/xml/elements/package-info.java
+++ src/main/java/com/brain/pulsar/xml/elements/package-info.java
@@ -5,9 +5,9 @@
  * @author Marshall Brain
  *
  */
-@XmlJavaTypeAdapter(value=StringTrimAdapter.class, type=String.class)
+@XmlJavaTypeAdapter(value=StringAdapter.class, type=String.class)
 package com.brain.pulsar.xml.elements;
 
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import com.brain.ion.xml.StringTrimAdapter;
+import com.brain.ion.xml.StringAdapter;
