021fc65424847ea1c7a2f37676b422813dcc6684
Marshall Brain <Marshall Brain@DESKTOP-7SIC72I>
2019-09-11 14:25:29 -0400

    Returns the position of the clicked district

--- src/main/java/com/brain/pulsar/ui/view/body/overview/Districts.java
+++ src/main/java/com/brain/pulsar/ui/view/body/overview/Districts.java
@@ -1,6 +1,8 @@
 package com.brain.pulsar.ui.view.body.overview;
 
+import java.awt.Point;
 import java.awt.Shape;
+import java.awt.geom.Area;
 import java.util.List;
 import java.util.Map;
 
@@ -17,9 +19,51 @@ import com.brain.pulsar.ui.view.RenderEntry;
 
 public class Districts {
 
-	public static void action(Mouse m) {
+	public static int action(Mouse m, Map<String, VectorGroup> vectorGroups) {
 		
+		if(m.buttonClicked(1)) {
+			
+			if(actionFrame(m, vectorGroups.get("districts"))) {
+				return actionEntry(m, vectorGroups.get("districts_entry"));
+			}
+			
+		}
 		
+		return -1;
+		
+	}
+	
+	private static boolean actionFrame(Mouse m, VectorGroup vg) {
+
+		Mouse mouseOffset = new Mouse(m, 150 + vg.getOrigin().x, 100 + vg.getOrigin().y);
+		
+		Vector frame = vg.getVectorById("frame");
+		Shape f = frame.getShape();
+		
+		Area clickable = new Area(f);
+		if(clickable.contains(mouseOffset.getPosition())) {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	private static int actionEntry(Mouse m, VectorGroup vg) {
+
+		Mouse mouseOffset = new Mouse(m, 150 + vg.getOrigin().x, 100 + vg.getOrigin().y);
+		
+		Vector frame = vg.getVectorById("frame");
+		Shape f = frame.getShape();
+		
+		Point offset = new Point(mouseOffset.getPosition().x, mouseOffset.getPosition().y%(f.getBounds().height+5));
+		
+		Area clickable = new Area(f);
+		if(clickable.contains(offset)) {
+			return mouseOffset.getPosition().y/(f.getBounds().height+5);
+		}
+		
+		return -1;
 		
 	}
 	
@@ -30,7 +74,7 @@ public class Districts {
 		
 	}
 	
-	public static void frame(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
+	private static void frame(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
 		
 		VectorGroup info = vectorGroups.get("districts");
 
@@ -56,24 +100,28 @@ public class Districts {
 		
 	}
 	
-	public static void entrys(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
+	private static void entrys(VectorGraphics g, Map<String, VectorGroup> vectorGroups, List<RenderEntry> shapes, Colony colony) {
 		
 		List<District> districts = colony.getDistricts();
+		
+		int x = 0;
 		
 		for(int i = 0; i < districts.size(); i++) {
 			
 			VectorGroup info = vectorGroups.get("districts_entry");
 
 			g.setTranslate(ScreenPosition.ZERO);
-			g.moveTranslate(150, 100 + i*55);
+			g.moveTranslate(150, 100 + x);
 			g.moveTranslate(info.getOrigin());
 			
 			Vector frame = info.getVectorById("frame");
 			Shape f = frame.getShape();
 			f = g.getAffineTransform().createTransformedShape(f);
+			x += f.getBounds().height+5;
 			
 			Vector icon = info.getVectorById("icon");
-			((Image) icon).setXform(g.getAffineTransform());
+			Shape b = icon.getShape();
+			b = g.getAffineTransform().createTransformedShape(b);
 			
 			Text text = (Text) info.getVectorById("amount");
 			text = text.setText(
@@ -82,7 +130,7 @@ public class Districts {
 			Shape t = text.getShape(g.getGraphics());
 			t = g.getAffineTransform().createTransformedShape(t);
 				
-			shapes.add(new RenderEntry(icon));
+			shapes.add(new RenderEntry(b, icon.getStyle()));
 			shapes.add(new RenderEntry(f, frame.getStyle()));
 			shapes.add(new RenderEntry(t, text.getStyle()));
 			
