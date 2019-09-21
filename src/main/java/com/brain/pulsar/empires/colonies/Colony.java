51ca2aa33429b145ea7f5df4fba6ec08eb63d1b6
Marshall Brain <Marshall Brain@DESKTOP-7SIC72I>
2019-09-20 22:45:53 -0400

    Decreased districts from 6 to 5

--- src/main/java/com/brain/pulsar/empires/colonies/Colony.java
+++ src/main/java/com/brain/pulsar/empires/colonies/Colony.java
@@ -34,7 +34,7 @@ public class Colony {
 		
 		parent = b;
 		
-		districts = new District[6];
+		districts = new District[5];
 		buildings = new ArrayList<>();
 		districtTypes = Collections.unmodifiableList(dt);
 		buildingTypes = Collections.unmodifiableList(bt);
