11dc0d7a6450189f6c1ebb365e0d469b7b1072b9
Marshall Brain <Marshall Brain@DESKTOP-7SIC72I>
2019-09-20 22:46:48 -0400

    Now renders a frame

--- src/main/java/com/brain/pulsar/ui/view/Detail.java
+++ src/main/java/com/brain/pulsar/ui/view/Detail.java
@@ -2,21 +2,33 @@ package com.brain.pulsar.ui.view;
 
 import java.util.Map;
 
+import com.brain.ion.graphics.ScreenPosition;
 import com.brain.ion.graphics.VectorGraphics;
+import com.brain.ion.graphics.vectors.Vector;
 import com.brain.ion.graphics.vectors.VectorGroup;
 
 public class Detail {
 	
-	private DetailInterface base;
+	private DetailInterface parent;
 	private Object target;
 
-	public Detail(DetailInterface base, Object target) {
-		this.base = base;
+	public Detail(DetailInterface parent, Object target) {
+		this.parent = parent;
 		this.target = target;
 	}
 
 	public void render(VectorGraphics g, Map<String, VectorGroup> vectorGroups) {
-		base.detailRender(g, vectorGroups, target);
+		
+		VectorGroup base = vectorGroups.get("base");
+
+		g.setTranslate(ScreenPosition.ZERO);
+		g.moveTranslate(150, 100);
+		g.moveTranslate(base.getOrigin());
+		
+		Vector frame = base.getVectorById("frame");
+		g.draw(frame.getShape(), frame.getStyle());
+		
+		parent.detailRender(g, vectorGroups, target);
 	}
 
 }
